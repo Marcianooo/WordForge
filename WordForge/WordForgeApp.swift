f83af552b65456a0f723cd8f371804ab6895401c
Marciano <italomarcianoaraujo@gmail.com>
2024-10-15 10:16:03 -0300

    Initial commit

---
+++ WordForge/WordForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordForgeApp.swift
+//  WordForge
+//
+//  Created by italo Marciano on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WordForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordForgeTests/WordForgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordForgeTests.swift
+//  WordForgeTests
+//
+//  Created by italo Marciano on 15/10/24.
+//
+
+import Testing
+@testable import WordForge
+
+struct WordForgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
