2269b557b4c85e517176ab0bfad6556506cbfddf
Marciano <italomarcianoaraujo@gmail.com>
2024-10-16 18:06:08 -0300

    Add two button and text center

---
+++ WordForge/NavigationBar/NavigationBarView.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationBarView.swift
+//  WordForge
+//
+//  Created by italo Marciano on 16/10/24.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationBarView()
+}
